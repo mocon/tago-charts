0c17d61acf81a4d3c52b8efe27124ccb150ae4eb
Myles O'Connor <myles.oconnor@gmail.com>
2023-11-15 16:01:11 -0800

    Example chart with dummy data

--- src/components/charts/ChartLine.tsx
+++ src/components/charts/ChartLine.tsx
@@ -21,27 +21,44 @@ ChartJS.register(
   Legend,
 )
 
-/** Colors for datasets in the chart */
-const colors = ['#E4D353', '#7CB2E9']
-
 type ChartLineProps = {
   data: TagoRealtimeData | null
 }
 
 /** Line chart, rendering data from Tago.io */
 export const ChartLine = ({ data }: ChartLineProps) => {
-  const labels = data?.map((item) => item.result.map((item) => item.time))
+  console.log('🏀 data =>', JSON.stringify(data, null, 2))
+
+  const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+  ]
 
   const chartData = {
     labels,
-    datasets: data?.map((item, index) => ({
-      label: item.result[0].metadata.label,
-      data: data[index].result.map((item) => item.value) || [],
-      borderColor: colors[index],
-      borderWidth: 1,
-      backgroundColor: 'transparent',
-      pointStyle: false,
-    })),
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: [0, 10, 5, 2, 20, 30, 45],
+        borderColor: '#E4D353',
+        borderWidth: 1,
+        backgroundColor: 'transparent',
+        pointStyle: false,
+      },
+      {
+        label: 'Dataset 2',
+        data: [10, 15, 25, 30, 32, 40, 50],
+        borderColor: '#7CB2E9',
+        borderWidth: 1,
+        backgroundColor: 'transparent',
+        pointStyle: false,
+      },
+    ],
   }
 
   const options = {
@@ -58,11 +75,9 @@ export const ChartLine = ({ data }: ChartLineProps) => {
     },
   }
 
-  if (!data) return null
-
   return (
     <div className='graph'>
-      {/* @ts-expect-error TODO: Fix `chartData` typing */}
+      {/* @ts-expect-error TODO: Fix this */}
       <Line options={options} data={chartData} />
     </div>
   )
